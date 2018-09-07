f603de9bb37f589aae7364283dad639c4c2a8282
sunil_kumar <sunil_kumar@seologistics.com>
2018-09-07 18:23:21 +0530

    changes in productin

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,9 +16,11 @@
 //= require all_function
 //= require_tree .
 
-// $(function(){
-// 	var faye = new Faye.Client('http://localhost:9292/faye')
-// 	faye.subscribe("/messages/new", function(data){
-// 		eval(data);
-// 	})
-// })
+$(function(){
+	// var faye = new Faye.Client('http://localhost:9292/faye')
+	var faye = new Faye.Client('http://ec2-18-222-195-118.us-east-2.compute.amazonaws.com/faye')
+
+	faye.subscribe("/messages/new", function(data){
+		eval(data);
+	})
+})
