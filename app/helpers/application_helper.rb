f18a36f8b8db7b1651dabc8525454ea17dec40a8
sunil_kumar <sunil_kumar@seologistics.com>
2018-09-10 09:58:42 +0530

    added link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,8 @@
 module ApplicationHelper
 	def broadcast(channel, &block)
 		  message = {:channel => channel, :data => capture(&block),:ext => {:auth_token => FAYE_TOKEN}}
-		  uri = URI.parse("http://localhost:9292/faye")
+		  # uri = URI.parse("http://localhost:9292/faye")
+		  uri = URI.parse("http://ec2-18-222-195-118.us-east-2.compute.amazonaws.com/faye")
 		  Net::HTTP.post_form(uri, :message => message.to_json)
 	end
 end
